539fada5920a8ba7918d08ea8e8c76783049187b
najlae01 <najlae.abarghache1@gmail.com>
2023-07-23 19:25:52 +0100

    SECOND commit

--- src/script.js
+++ src/script.js
@@ -23,7 +23,7 @@ const scene = new THREE.Scene()
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 128)
 
 // Color
 debugObject.depthColor = '#0b0b3d'
@@ -39,6 +39,11 @@ const waterMaterial = new THREE.ShaderMaterial({
     uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
     uBigWavesSpeed: { value: 0.75 },
 
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallWavesIterations: { value: 3 },
+
     uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
     uColorOffset: { value: 0.15 },
@@ -51,29 +56,57 @@ gui
   .add(waterMaterial.uniforms.uBigWavesElevation, 'value')
   .min(0)
   .max(1)
-  .step(0.01)
+  .step(0.001)
   .name('uBigWavesElevation')
 
 gui
   .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
   .min(0)
   .max(10)
-  .step(0.01)
+  .step(0.001)
   .name('uBigWavesFrequencyX')
 
 gui
   .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
   .min(0)
   .max(10)
-  .step(0.01)
+  .step(0.001)
   .name('uBigWavesFrequencyY')
 
 gui
   .add(waterMaterial.uniforms.uBigWavesSpeed, 'value')
   .min(0)
   .max(4)
-  .step(0.01)
+  .step(0.001)
   .name('uBigWavesSpeed')
+
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name('uSmallWavesElevation')
+
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name('uSmallWavesFrequency')
+
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name('uSmallWavesSpeed')
+
+gui
+  .add(waterMaterial.uniforms.uSmallWavesIterations, 'value')
+  .min(0)
+  .max(5)
+  .step(1)
+  .name('uSmallWavesIterations')
 
 gui
   .addColor(debugObject, 'depthColor')
